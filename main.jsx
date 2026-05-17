88e4308722da6a86633ebf514a72d997338e4d13
NEXORA <alighaderi5@gmail.com>
2026-05-17 09:36:59 -0400

    Fix typo in appId assignment

--- main.jsx
+++ main.jsx
@@ -62,7 +62,7 @@ function LoginPage() {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PrivyProvider
-    appId="cmp95s2uh00w40cjrnma5b8k3"
+    appId=appId="cmp95s2uh00w40cjrnma5b8k3"
     config={{
       loginMethods: ["wallet"],
       appearance: {
